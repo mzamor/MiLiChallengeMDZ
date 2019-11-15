187c37470dba91d46d10249c22dd9a86ee4c594e
madaza <zamoramariano@hotmail.com>
2019-11-15 10:14:09 -0300

    cambios en el get

--- app/src/main/java/com/example/milichallenge/presentation/service/SearchServices.kt
+++ app/src/main/java/com/example/milichallenge/presentation/service/SearchServices.kt
@@ -4,8 +4,9 @@ import com.example.milachallenge.presentation.main.adapter.model.ResultSearch
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface SearchServices {
-    @GET("/{capital}")
-    fun getSearchProducts(@Path("iphone") productSearch: String): Call<ResultSearch>
+    @GET("/search")
+    fun getSearchProducts(@Query("q") productSearch: String): Call<ResultSearch>
 }
