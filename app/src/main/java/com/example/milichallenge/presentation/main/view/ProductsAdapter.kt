e164f4a6c6e0c8027466755c0aa4c2fecc80f403
madaza <zamoramariano@hotmail.com>
2019-11-23 13:07:40 -0300

    fix en caso donde no hay installments

--- app/src/main/java/com/example/milichallenge/presentation/main/view/ProductsAdapter.kt
+++ app/src/main/java/com/example/milichallenge/presentation/main/view/ProductsAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.milichallenge.presentation.main.view
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
@@ -62,18 +63,12 @@ class ProductsAdapter(private val context: Context, var listener: ClickListener)
         fun bind(context: Context, product: Product, listener: ClickListener) {
             Picasso.get().load(product.thumbnail).into(itemView.iv_product)
             itemView.tv_title.text = product.title
-            itemView.tv_price.text = String.format(
-                context.getString(R.string.price),
-                product.price.roundToInt().toString()
-            )
-            itemView.tv_installments.text = String.format(
-                context.getString(R.string.installments),
-                product.installments.quantity.toString()
-            )
-            itemView.tv_installments.visibility =
-                if ((product.installments.quantity > 0) && (product.installments.rate <= 0)) VISIBLE else GONE
-            itemView.tv_free_shipping.visibility =
-                if (product.shipping.freeShipping) VISIBLE else GONE
+            itemView.tv_price.text = String.format(context.getString(R.string.price), product.price.roundToInt().toString())
+            if(product.installments != null){
+                Log.e("quantity",product.installments.quantity.toString())
+                itemView.tv_installments.text = String.format(context.getString(R.string.installments), product.installments.quantity.toString())
+                itemView.tv_free_shipping.visibility = if (product.shipping.freeShipping) VISIBLE else GONE
+            }
             itemView.rb_stars_number.setRating(product.getStar().toFloat())
             itemView.tv_comments_number.text = product.getCommentNumber().toString()
             itemView.setOnClickListener(View.OnClickListener {
